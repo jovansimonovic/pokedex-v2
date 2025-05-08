f7b44ecf3399b4686005d01e9c51a97000811215
Jovan Simonovic <jovan.simonovic02@gmail.com>
2025-05-08 16:33:59 +0200

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router";
+import pokeball from "../assets/images/pokeball.webp";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink to={"/"} className="logo">
+        <img src={pokeball} width={30} height={30} alt="logo image" />
+        <span>Pokédex</span>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/pokemon"}>Pokémon</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/about"}>About</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
